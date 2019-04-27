526381e1d0b8f1465b2044cee921258b12c4f5e0
Alexander <alexander@MacBook-Pro-Alexander.local>
2019-04-27 15:05:59 +0300

    Multiple selection on click and button

--- src/components/Modal/NothingSelected.js
+++ src/components/Modal/NothingSelected.js
@@ -7,7 +7,7 @@ const NothingSelected = (props) => {
       <div className="header">
         <h3>No file or directory selected</h3>
       </div>
-      <div className="modal-footer lower">
+      <div className="modal-footer nothing-selected">
         <button type="button" className="btn btn-danger cancel" onClick={props.closeModal}>Close</button>
       </div>
     </div>
