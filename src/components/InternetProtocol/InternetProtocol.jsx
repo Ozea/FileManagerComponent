6adddf85cc9b733b40cb85defd51276bf32fdf7f
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-09-15 11:12:23 +0300

    Implememted IP list hotkeys

--- src/components/InternetProtocol/InternetProtocol.jsx
+++ src/components/InternetProtocol/InternetProtocol.jsx
@@ -4,56 +4,72 @@ import Container from '../ControlPanel/Container/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './InternetProtocol.scss';
 
-class InternetProtocol extends Component {
-  toggleFav = (starred) => {
+const InternetProtocol = props => {
+  const { data } = props;
+  const { i18n } = window.GLOBAL.App;
+  const token = localStorage.getItem("token");
+
+  const toggleFav = (starred) => {
     if (starred) {
-      this.props.toggleFav(this.props.data.NAME, 'add');
+      props.toggleFav(data.NAME, 'add');
     } else {
-      this.props.toggleFav(this.props.data.NAME, 'delete');
+      props.toggleFav(data.NAME, 'delete');
     }
   }
 
-  checkItem = () => {
-    this.props.checkItem(this.props.data.NAME);
+  const checkItem = () => {
+    props.checkItem(data.NAME);
   }
 
-  render() {
-    const { data } = this.props;
-    const { i18n } = window.GLOBAL.App;
-    const token = localStorage.getItem("token");
+  const handleDelete = () => {
+    props.handleModal(data.delete_conf, `/delete/ip?ip=${data.NAME}&token=${token}`);
+  }
 
-    return (
-      <ListItem checked={data.isChecked} date={data.DATE} starred={data.STARRED} toggleFav={this.toggleFav} checkItem={this.checkItem} >
-        <Container className="r-col w-85">
-          <div className="name">{data.NAME}</div>
-          <br />
-          <div className="stats">
-            <Container className="c-1 w-35">
-              <div className="ip"><span className="stat">{data.NETMASK}</span></div>
-              <div className="soa"><span className="stat">{data.INTERFACE}</span></div>
-            </Container>
-            <Container className="c-2 w-30">
-              <div>{i18n.Domains}: <span className="stat">{data.U_WEB_DOMAINS}</span></div>
-              <div>{i18n.Status}: <span className="stat">{data.STATUS}</span></div>
-            </Container>
-            <Container className="c-3 w-35">
-              <div>{i18n.Owner}: <span className="stat">{data.OWNER}</span></div>
-              <div>{i18n.Users}: <span className="stat">{data.U_SYS_USERS.replace(',', ', ')}</span></div>
-            </Container>
-          </div>
-        </Container>
-        <div className="actions">
-          <div><a className="link-edit" href={`/edit/ip/?ip=${data.IP}`}>{i18n.edit} <FontAwesomeIcon icon="pen" /></a></div>
-          <div>
-            <button className="link-delete" onClick={() => this.props.handleModal(data.delete_conf, `/delete/ip?ip=${data.NAME}&token=${token}`)}>
-              {i18n.Delete}
-              <FontAwesomeIcon icon="times" />
-            </button>
-          </div>
+  return (
+    <ListItem
+      id={data.NAME}
+      focused={data.FOCUSED}
+      checked={data.isChecked}
+      date={data.DATE}
+      starred={data.STARRED}
+      toggleFav={toggleFav}
+      checkItem={checkItem}>
+
+      <Container className="r-col w-85">
+        <div className="name">{data.NAME}</div>
+        <br />
+        <div className="stats">
+          <Container className="c-1 w-35">
+            <div className="ip"><span className="stat">{data.NETMASK}</span></div>
+            <div className="soa"><span className="stat">{data.INTERFACE}</span></div>
+          </Container>
+          <Container className="c-2 w-30">
+            <div>{i18n.Domains}: <span className="stat">{data.U_WEB_DOMAINS}</span></div>
+            <div>{i18n.Status}: <span className="stat">{data.STATUS}</span></div>
+          </Container>
+          <Container className="c-3 w-35">
+            <div>{i18n.Owner}: <span className="stat">{data.OWNER}</span></div>
+            <div>{i18n.Users}: <span className="stat">{data.U_SYS_USERS.replace(',', ', ')}</span></div>
+          </Container>
         </div>
-      </ListItem>
-    );
-  }
+      </Container>
+      <div className="actions">
+        <div>
+          <a className="link-edit" href={`/edit/ip/?ip=${data.NAME}`}>
+            {i18n.edit}
+            {data.FOCUSED ? <span className="shortcut-button html-unicode">&#8617;</span> : <FontAwesomeIcon icon="pen" />}
+          </a>
+        </div>
+
+        <div>
+          <button className="link-delete" onClick={() => handleDelete()}>
+            {i18n.Delete}
+            {data.FOCUSED ? <span className="shortcut-button del">Del</span> : <FontAwesomeIcon icon="times" />}
+          </button>
+        </div>
+      </div>
+    </ListItem>
+  );
 }
 
 export default InternetProtocol;
