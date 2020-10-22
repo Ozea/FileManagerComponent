d4c98b58ebb36903b40439996bd13dde6ce1c054
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-10-22 19:02:03 +0300

    Created database add form

--- src/ControlPanelService/Db.js
+++ src/ControlPanelService/Db.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 const BASE_URL = window.location.origin;
 const webApiUri = '/list/db/db.php';
+const addDbApiUri = '/api/add/db/index.php';
+const optionalDbInfoUri = '/api/add/db/index.php';
 
 export const getDatabaseList = () => {
   return axios.get(BASE_URL + webApiUri);
@@ -25,3 +27,56 @@ export const bulkAction = (action, domainNameSystems) => {
 export const handleAction = uri => {
   return axios.get(BASE_URL + uri);
 }
+
+export const getDbOptionalInfo = () => {
+  return axios.get(BASE_URL + optionalDbInfoUri);
+}
+
+export const addDatabase = data => {
+  let formDataObject = new FormData();
+
+  for (let key in data) {
+    formDataObject.append(key, data[key]);
+  }
+
+  return axios.post(BASE_URL + addDbApiUri, formDataObject);
+}
+
+export const dbCharsets = [
+  'big5',
+  'dec8',
+  'cp850',
+  'hp8',
+  'koi8r',
+  'latin1',
+  'latin2',
+  'swe7',
+  'ascii',
+  'ujis',
+  'sjis',
+  'hebrew',
+  'tis620',
+  'euckr',
+  'koi8u',
+  'gb2312',
+  'greek',
+  'cp1250',
+  'gbk',
+  'latin5',
+  'armscii8',
+  'utf8',
+  'ucs2',
+  'cp866',
+  'keybcs2',
+  'macce',
+  'macroman',
+  'cp852',
+  'latin7',
+  'cp1251',
+  'cp1256',
+  'cp1257',
+  'binary',
+  'geostd8',
+  'cp932',
+  'eucjpms'
+];
