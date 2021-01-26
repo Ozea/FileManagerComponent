124999ed797221a2c54e431c2fc320a9a5f8be26
Alexander <alexander@MacBook-Pro-Alexander.local>
2021-01-26 17:50:26 +0200

    Implemented Edit DNS Record form layout

---
+++ src/containers/EditDNSWrapper/EditDNSWrapper.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import EditDomainNameSystem from 'src/components/DomainNameSystem/Edit/EditDomainNameSystem';
+import EditDNSRecord from 'src/components/DNSRecord/Edit/EditDNSRecord';
+import { useHistory } from 'react-router-dom';
+import QueryString from 'qs';
+
+export default function EditDNSWrapper() {
+  const history = useHistory();
+  const parsedQueryString = QueryString.parse(history.location.search, { ignoreQueryPrefix: true });
+  const [isDnsRecord, setIsDnsRecord] = useState(false);
+
+  useEffect(() => {
+    if (parsedQueryString.domain) {
+      setIsDnsRecord(true);
+    } else {
+      setIsDnsRecord(false);
+    }
+  }, [history.location]);
+
+  return (
+    <>
+      {
+        isDnsRecord
+          ? <EditDNSRecord domain={parsedQueryString.domain} />
+          : <EditDomainNameSystem />
+      }
+    </>
+  );
+}
